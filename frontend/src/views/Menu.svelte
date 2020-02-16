62f4cae43c2a5234eda0205a1717427c3f5858f1
jemerylim <s10179911@connect.np.edu.sg>
2020-02-16 22:47:03 +0800

    add category to food item and change search

--- frontend/src/views/Menu.svelte
+++ frontend/src/views/Menu.svelte
@@ -1,5 +1,11 @@
 <script>
-  import { getFoods, deleteFood,getSetMenus,getSetItems } from "../api";
+  import {
+    getFoods,
+    deleteFood,
+    getSetMenus,
+    getSetItems,
+    getCategorys
+  } from "../api";
   import { Link, navigate } from "svelte-routing";
   import { onMount } from "svelte";
   import { user, selectedFood } from "./../stores.js";
@@ -8,34 +14,44 @@
   import { Confirm } from "svelte-confirm";
 
   let foods = [];
+  let category = [];
+  let categoryDisplayed = [];
   let setMenu = [];
   let foodsDisplayed = [];
   let setMenuDisplayed = [];
   let setItemDisplayed = [];
   let id = 0;
   let dialogOpen = false;
-   let queryTextForSetmenu = "";
+  let queryTextForSetmenu = "";
 
-  let queryTextForFood = "";
+  let queryTextForCategory = "";
   onMount(() => {
     getFoods()
       .then(r => r.json())
       .then(response => {
         foods = response.result;
         foodsDisplayed = response.result;
+        console.log(foods)
+      });
+    getCategorys()
+      .then(r => r.json())
+      .then(response => {
+        category = response.result;
+        categoryDisplayed = response.result;
+        console.log(response);
       });
     getSetMenus()
       .then(r => r.json())
       .then(response => {
-        setMenu = response.result
+        setMenu = response.result;
         setMenuDisplayed = response.result;
         //console.log(setMenuDisplayed)
       });
-     getSetItems()
+    getSetItems()
       .then(r => r.json())
       .then(response => {
         setItemDisplayed = response;
-        console.log(response)
+        console.log(response);
       });
   });
 
@@ -59,13 +75,13 @@
       });
   }
 
-  function searchFood() {
-    console.log(queryTextForFood);
+  function searchCategory() {
+    console.log(queryTextForCategory);
 
-    if (queryTextForFood === "") foodsDisplayed = foods;
+    if (queryTextForCategory === "") categoryDisplayed = category;
     else
-      foodsDisplayed = foods.filter(e =>
-        e.name.toUpperCase().includes(queryTextForFood.toUpperCase())
+      categoryDisplayed = category.filter(e =>
+        e.name.toUpperCase().includes(queryTextForCategory.toUpperCase())
       );
   }
   function searchSetMenu() {
@@ -99,14 +115,14 @@
 
 <h1>Food Menu</h1>
 <div class="filter">
-  <label for="name">Food Name:</label>
+  <label for="name">Category Name:</label>
   <input
     type="text"
-    on:keyup={searchFood}
-    placeholder="Search for Food Item.."
-    title="Type in a Food name"
+    on:keyup={searchCategory}
+    placeholder="Search for Category.."
+    title="Type in a Category"
     id="searchFood"
-    bind:value={queryTextForFood} />
+    bind:value={queryTextForCategory} />
 </div>
 <table id="foodTable">
   <tr>
@@ -119,59 +135,69 @@
     {/if}
     <th>action</th>
   </tr>
-  {#if foodsDisplayed != []}
-    {#each foodsDisplayed as food}
+  {#if categoryDisplayed != []}
+    {#each categoryDisplayed as cat}
       <tr>
-        <td class="tableData">{food.name}</td>
-        <td class="tableData">{food.description}</td>
-        <td class="tableData">{food.price}</td>
-        {#if $user.role === 'manager'}
-          <td class="tableData">{food.unit}</td>
-          <td class="tableData">{food.status}</td>
-        {/if}
-        <td>
-          <div>
-            {#if $user.role === 'customer'}
-              <button>
-                <img src="./../../public/cart.png" alt="Add to Cart" />
-              </button>
-            {/if}
-            {#if $user.role === 'manager'}
-              <button on:click={() => updateFood(food)}>
-                <img src="./../../public/edit.png" alt="Update" />
-              </button>
-              <Confirm
-                confirmTitle="Delete"
-                cancelTitle="Cancel"
-                let:confirm={confirmThis}>
-                <svg
-                  style="width:24px;height:24px"
-                  viewBox="0 0 24 24"
-                  class="delete-icon"
-                  on:click={() => confirmThis(deactiveFoodItem, food)}>
-                  <path
-                    fill="hsl(200, 40%, 20%)"
-                    d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0
-                    8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
-                </svg>
-                <span slot="title">Delete this item?</span>
-                <span slot="description">
-                  You won't be able to revert this!
-                </span>
-              </Confirm>
-            {/if}
-          </div>
+        <td colspan="6" class="tableData" style="text-align: center">
+           {cat.name} category
         </td>
       </tr>
+      {#if foodsDisplayed != []}
+        {#each foodsDisplayed as food}
+          {#if food.category == cat.id}
+          <tr>
+            <td class="tableData">{food.name}</td>
+            <td class="tableData">{food.description}</td>
+            <td class="tableData">{food.price}</td>
+            {#if $user.role === 'manager'}
+              <td class="tableData">{food.unit}</td>
+              <td class="tableData">{food.status}</td>
+            {/if}
+            <td>
+              <div>
+                {#if $user.role === 'customer'}
+                  <button>
+                    <img src="./../../public/cart.png" alt="Add to Cart" />
+                  </button>
+                {/if}
+                {#if $user.role === 'manager'}
+                  <button on:click={() => updateFood(food)}>
+                    <img src="./../../public/edit.png" alt="Update" />
+                  </button>
+                  <Confirm
+                    confirmTitle="Delete"
+                    cancelTitle="Cancel"
+                    let:confirm={confirmThis}>
+                    <svg
+                      style="width:24px;height:24px"
+                      viewBox="0 0 24 24"
+                      class="delete-icon"
+                      on:click={() => confirmThis(deactiveFoodItem, food)}>
+                      <path
+                        fill="hsl(200, 40%, 20%)"
+                        d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0
+                        8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
+                    </svg>
+                    <span slot="title">Delete this item?</span>
+                    <span slot="description">
+                      You won't be able to revert this!
+                    </span>
+                  </Confirm>
+                {/if}
+              </div>
+            </td>
+          </tr>
+          {/if}
+        {/each}
+      {:else}
+        <tr>
+          <td colspan="100%">
+            <h5 class="text-center">There are no Foods.</h5>
+          </td>
+        </tr>
+      {/if}
     {/each}
-  {:else}
-    <tr>
-      <td colspan="100%">
-        <h5 class="text-center">There are no Foods.</h5>
-      </td>
-    </tr>
   {/if}
-
 </table>
 
 <h1>Set Menu</h1>
@@ -190,54 +216,59 @@
     <th>Set Name</th>
     <th>price</th>
     <th>size</th>
-     {#if $user.role === 'customer'}
-    <th>action</th>
+    {#if $user.role === 'customer'}
+      <th>action</th>
     {/if}
   </tr>
   {#if setMenuDisplayed != []}
     {#each setMenuDisplayed as setMenu}
       <tr>
-        <td colspan="5" class="tableData" style="text-align: center">{setMenu.name}</td>
-        {#each setItemDisplayed as setItem}
-          {#if setItem.setmenu_id == setMenu.id}
-          <td class="tableData">{setItem.name}</td>
-        <td class="tableData">{setItem.totalPrice}</td>
-        <td class="tableData">{setItem.size}</td>
-         <td>
-          <div>
-            {#if $user.role === 'customer'}
-              <button>
-                <img src="./../../public/cart.png" alt="Add to Cart" />
-              </button>
-            {/if}
-            {#if $user.role === 'manager'}
-              <button >
-                <img src="./../../public/edit.png" alt="Update" />
-              </button>
-              <Confirm
-                confirmTitle="Delete"
-                cancelTitle="Cancel"
-                let:confirm={confirmThis}>
-                <svg
-                  style="width:24px;height:24px"
-                  viewBox="0 0 24 24"
-                  class="delete-icon"}>
-                  <path
-                    fill="hsl(200, 40%, 20%)"
-                    d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0
-                    8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
-                </svg>
-                <span slot="title">Delete this item?</span>
-                <span slot="description">
-                  You won't be able to revert this!
-                </span>
-              </Confirm>
-              {/if}
-          </div>
-        </td> 
-        {/if}
-        {/each}
+        <td colspan="5" class="tableData" style="text-align: center">
+          {setMenu.name}
+        </td>
       </tr>
+      {#each setItemDisplayed as setItem}
+        {#if setItem.setmenu_id == setMenu.id}
+        <tr>
+          <td class="tableData">{setItem.name}</td>
+          <td class="tableData">{setItem.totalPrice}</td>
+          <td class="tableData">{setItem.size}</td>
+          <td>
+            <div>
+              {#if $user.role === 'customer'}
+                <button>
+                  <img src="./../../public/cart.png" alt="Add to Cart" />
+                </button>
+              {/if}
+              {#if $user.role === 'manager'}
+                <button>
+                  <img src="./../../public/edit.png" alt="Update" />
+                </button>
+                <Confirm
+                  confirmTitle="Delete"
+                  cancelTitle="Cancel"
+                  let:confirm={confirmThis}>
+                  <svg
+                    style="width:24px;height:24px"
+                    viewBox="0 0 24 24"
+                    class="delete-icon"
+                    }>
+                    <path
+                      fill="hsl(200, 40%, 20%)"
+                      d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0
+                      8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
+                  </svg>
+                  <span slot="title">Delete this item?</span>
+                  <span slot="description">
+                    You won't be able to revert this!
+                  </span>
+                </Confirm>
+              {/if}
+            </div>
+          </td>
+          </tr>
+        {/if}
+      {/each}
     {/each}
   {:else}
     <tr>
@@ -248,4 +279,3 @@
   {/if}
 
 </table>
-
